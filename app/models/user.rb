aa3a716f24d1ebb4a37c2d2e474d04197a5d2e11
池上将希 <masaki@ikegamimasakinoMacBook-Pro.local>
2021-03-18 20:33:41 +0900

    設定追加

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable,
+         :omniauthable, omniauth_providers: %i[qiita]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.username = auth.info.nickname
+      user.password = Devise.friendly_token[0, 20]
+      user.image = auth.info.image 
+      user.access_token = auth.credentials.token
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  root to: 'home#index'
+  devise_for :users, skip: [:registration, :session, :password], controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_scope :user do
+    get    '/users/sign_in',  to: 'home#index',              as: :new_user_session
+    get    '/users/sign_up',  to: 'home#index',              as: :new_user_registration
+    delete '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+  
 end
