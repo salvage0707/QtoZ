9353999c28ed631bceea00a8a3943fee5e827370
池上将希 <masaki@ikegamimasakinoMacBook-Pro.local>
2021-03-19 02:41:13 +0900

    tailwind のconfig読み込み不具合解消

--- app/javascript/packs/application/header.js
+++ app/javascript/packs/application/header.js
@@ -1,6 +1,24 @@
+import '../../css/application/header.css'
+
 $(() => {
   // ヘッダーのユーザーメニュー切り替え
   $('#user-menu-button').on('click', () => {
     $('#user-menu').toggleClass('hidden');
   });
+
+  // ログインのモーダル
+  $('.sign-in-modal-button').on('click', (e) => {
+    e.preventDefault();
+    toggleModal();
+  });
+  $('.modal-overlay').on('click',toggleModal);
+  $('.modal-close').on('click',toggleModal);
+
+  function toggleModal () {
+    let modal = $('.sign-in-modal');
+    modal.toggleClass('opacity-0');
+    modal.toggleClass('pointer-events-none');
+    modal.toggleClass('modal-active');
+  }
 });
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
   root to: 'home#index'
+
   devise_for :users, skip: [:registration, :session, :password], controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    get    '/users/sign_in',  to: 'home#index',              as: :new_user_session
+    get    '/users/sign_in',  to: 'devise/sessions#new',              as: :new_user_session
     get    '/users/sign_up',  to: 'home#index',              as: :new_user_registration
     delete '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
-  
 end
