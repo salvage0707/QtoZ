3b6e9f63a2c413e5b90671e731804fb14984b9a3
池上将希 <masaki@ikegamimasakinoMacBook-Pro.local>
2021-03-22 00:01:12 +0900

    base戻す

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+
+file = File.read(Rails.root.join('tmp', 'data', 'articles.json'))
+data_hash = JSON.parse(file)
+data_hash.each do |data|
+  title = data["title"]
+  qiita_uid = data["id"]
+  url = data["url"]
+  tag_names = data["tags"].map { |item| item["name"] }
+  tags = tag_names.join(",")
+  isPrivate = data["private"]
+  created_at = data["created_at"]
+  Article.create!(title: title,slag:qiita_uid, emoji:"✊", category:"tech", topics:tags, qiita_uid: qiita_uid, qiita_url: url, qiita_created_at: created_at, user_id: 1)
+end
