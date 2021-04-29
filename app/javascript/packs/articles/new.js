b26bcfb1782122d4d96ec3208377ff46f3a26786
池上将希 <masaki@ikegamimasakinoMacBook-Pro.local>
2021-04-29 17:57:02 +0900

    絵文字入力機能削除

--- app/javascript/packs/articles/new.js
+++ app/javascript/packs/articles/new.js
@@ -1,12 +1,1 @@
 import '../../css/articles/new.css';
-import '../../css/twemoji-picker.css';
-import '../lib/twemoji-picker';
-
-console.log('きたよ')
-
-$("#emoji").twemojiPicker({
-  height: '50px',
-  pickerWidth: '300px',
-  categorySize: '40px',
-  size: '40px'
-})
