d6349c78d01a8f98afe19da383646f0cfe670f6e
池上将希 <masaki@ikegamimasakinoMacBook-Pro.local>
2021-05-08 00:50:29 +0900

    rspecにしているため、testディレクトリを削除 #9

--- test/controllers/zip_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ZipControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get zip_index_url
-    assert_response :success
-  end
-end
--- test/models/zip_job_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class ZipJobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
