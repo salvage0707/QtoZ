e14e8dbe4e43cb739a35bbe13e928244d8a5e67f
池上将希 <masaki@ikegamimasakinoMacBook-Pro.local>
2021-05-10 17:56:02 +0900

    不要コード削除

--- app/javascript/packs/articles/new.js
+++ app/javascript/packs/articles/new.js
@@ -1,9 +1,1 @@
 import '../../css/articles/new.css';
-
-$( "form" ).on( "submit", function( event ) {
-  event.preventDefault();
-  let data = JSON.stringify( $(this).serializeArray() ); 
-  console.log( data );
-});
-
-
